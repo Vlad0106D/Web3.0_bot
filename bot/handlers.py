b5b258ea0d0689d75a14cbd7798bbf49f60f1df0
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 06:32:47 +0200

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,17 +1,13 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
+from bot.commands.check import check_command
 
-from bot.commands.check import check_command  # этот файл должен быть
-
-# Команда /start
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Привет! Я Web3 трейдинг-бот.")
 
-# Команда /help
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🛠 Команды: /start, /check, /help")
 
-# Регистрируем команды
 def setup_handlers(app):
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("help", help_command))
