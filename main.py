7b917fc4f745f9d1aa961f05971a4294b6beef9a
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 08:25:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import os
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
 
-# Получаем токен из переменной окружения
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
-
     application.bot.delete_webhook(drop_pending_updates=True)
-
     setup_handlers(application)
-
     print("✅ Бот запущен через polling...")
     application.run_polling()
 
