cc8470363c578b697d93b6cae207b574e08c90eb
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 05:16:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-# main.py
-
 import asyncio
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
 from config import TELEGRAM_BOT_TOKEN
 
+from quart import Quart
+
+# Инициализируем Quart
+app_web = Quart(__name__)
+
+@app_web.route('/')
+async def index():
+    return "Web3 Bot is running"
+
+async def start_bot():
+    bot_app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    setup_handlers(bot_app)
+    print("Telegram bot is running...")
+    await bot_app.run_polling()
+
+async def start_web():
+    print("Web server is running...")
+    await app_web.run_task(host="0.0.0.0", port=8080)
+
 async def main():
-    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
-    setup_handlers(app)
-    print("Bot is running...")
-    await app.run_polling()
+    await asyncio.gather(
+        start_bot(),
+        start_web()
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
