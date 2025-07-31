379c45377433470a382cab7cfffef549de2711b6
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 06:00:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import asyncio
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
 from config import TELEGRAM_BOT_TOKEN
 
-def main():
+async def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     setup_handlers(app)
-    print("Бот запущен")
-    app.run_polling()
+
+    # 💥 Удаляем вебхук перед polling, иначе Telegram даст Conflict
+    await app.bot.delete_webhook(drop_pending_updates=True)
+
+    print("Бот запущен...")
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
