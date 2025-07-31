25972be5d06d198224adac4b48085a1537ceec77
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 04:14:54 +0200

    Update coingeko.py

--- services/coingeko.py
+++ services/coingeko.py
@@ -1,1 +1,19 @@
+# services/coingecko.py
 
+import httpx
+from config import VS_CURRENCY
+
+async def get_market_data(coin_id):
+    url = f"https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": VS_CURRENCY,
+        "ids": coin_id,
+    }
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            if response.status_code == 200 and response.json():
+                return response.json()[0]
+    except Exception as e:
+        print(f"Ошибка при запросе данных CoinGecko: {e}")
+    return None
