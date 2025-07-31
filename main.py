9d705d6bb8494a1f05edc528c2a112d735d6b1ea
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 08:20:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import os
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
-from config import TELEGRAM_BOT_TOKEN
+
+# Получаем токен из переменной окружения
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # Удаляем Webhook, если остался
     application.bot.delete_webhook(drop_pending_updates=True)
 
-    # Подключаем команды
     setup_handlers(application)
 
-    print("✅ Бот запущен через polling (финально, без asyncio.run)...")
-
-    # Запускаем polling (синхронно, без asyncio.run)
+    print("✅ Бот запущен через polling...")
     application.run_polling()
 
 if __name__ == "__main__":
