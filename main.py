a1c5b9ebfe6d5068b765d037063c1b4f439e9467
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 09:56:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main.py
 
+import os
 import asyncio
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
-from config import TELEGRAM_TOKEN
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def main():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
@@ -11,5 +13,14 @@ async def main():
     print("✅ Бот запущен через polling...")
     await application.run_polling()
 
+# Проверка на уже запущенный event loop
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+        else:
+            raise
