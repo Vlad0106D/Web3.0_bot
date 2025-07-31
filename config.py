faf47a03b81c591d5b29f57b9afb45b0d0d938da
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 04:50:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-# config.py
-
 import os
+from dotenv import load_dotenv
 
+# Загружаем переменные из .env
+load_dotenv()
+
+# Получаем токен из переменной окружения
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# Остальная конфигурация
 PAIRS = ["bitcoin", "ethereum"]
 VS_CURRENCY = "usd"
