3813df1830c5f67f5e3ccc40c9be693faa49c7c5
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 09:13:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import os
-import asyncio
 from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
+from config import TELEGRAM_BOT_TOKEN
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-async def run_bot():
+def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # Подключаем команды
+    # Подключаем обработчики
     setup_handlers(application)
-
-    # Удаляем webhook
-    await application.bot.delete_webhook(drop_pending_updates=True)
 
     print("✅ Бот запущен через polling...")
 
-    # Запускаем бота
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.updater.idle()
-
+    # Синхронный запуск без async (не использовать asyncio.run)
+    application.run_polling()
+    
 if __name__ == "__main__":
-    asyncio.run(run_bot())
+    main()
