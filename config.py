55f771b5bbe1692b21150a17bd78553fc2f91f2e
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 10:12:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
 # config.py
 
-TELEGRAM_TOKEN = "7753750626:AAF17RQv-ySPqppsBMF_crvHw6p-ZNwMwtU"
-
-# список пар для анализа
-SYMBOLS = [
-    "BTCUSDT", "ETHUSDT", "SOLUSDT", "XRPUSDT", "SUIUSDT",
-    "APTUSDT", "ARBUSDT", "OPUSDT", "STXUSDT", "TIAUSDT"
-]
+TOKEN = "7753750626:AAF17RQv-ySPqppsBMF_crvHw6p-ZNwMwtU"
