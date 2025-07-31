f89afdcb402a4b4b7593daaf92393dcb08f0b1ad
Vlad0106D <vladmarunich22@icloud.com>
2025-07-31 04:13:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# main.py
 
+import asyncio
+from telegram.ext import ApplicationBuilder
+from bot.handlers import setup_handlers
+from config import TELEGRAM_BOT_TOKEN
+
+async def main():
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    setup_handlers(app)
+    print("Bot is running...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
